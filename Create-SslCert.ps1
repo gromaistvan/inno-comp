45f20a778d011f2191797f87f3e34cce9cf7e18a
Groma István <Groma.IstvanDr@sdadms.hu>
2020-10-16 21:47:17 +0200

    Remove junk

--- Create-SslCert.ps1
+++
@@ -1,7 +0,0 @@
-param ($domain)
-$short = ($domain -split '\.')[0]
-$password = ConvertTo-SecureString -String 'bubuka' -Force -AsPlainText
-$3years = (Get-Date).AddYears(3)
-$certificate = New-SelfSignedCertificate -FriendlyName $short -DnsName $domain, $short -NotAfter $3years -KeyExportPolicy Exportable -CertStoreLocation cert:\LocalMachine\My
-$thumbprint = $certificate.Thumbprint
-Get-ChildItem -Path cert:\LocalMachine\My\$thumbprint | Export-PfxCertificate -FilePath "$short.pfx" -Password $password
