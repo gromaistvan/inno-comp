5da9d8be5923ea189ddf0cbca5c3547ecf703e32
gromaistvan <wavezone@mrginfo.com>
2020-10-23 19:03:30 +0200

    Restructor

--- back/server.js
+++ back/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const app = express(),
-      bodyParser = require("body-parser");
-      port = 3080;
+const bodyParser = require("body-parser");
 
+const port = 3080;
 const users = [];
+const app = express();
 
 app.use(bodyParser.json());
-app.use(express.static(process.cwd() + "/app/"));
+app.use(express.static(`${process.cwd()}/app/`));
 
 app.get('/api/users', (req, res) => {
   res.json(users);
@@ -18,10 +18,10 @@ app.post('/api/user', (req, res) => {
   res.json("user addedd");
 });
 
-app.get('/', (req,res) => {
-  res.sendFile(process.cwd()+"/my-app/dist/angular-nodejs-example/index.html")
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + "/my-app/dist/angular-nodejs-example/index.html")
 });
 
 app.listen(port, () => {
-    console.log(`Server listening on the port::${port}`);
+  console.log(`Server listening on the port ${port}.`);
 });
