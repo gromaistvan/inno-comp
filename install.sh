d2ae685100c457d21c13adb958230625b41eb074
Groma István <Groma.IstvanDr@sdadms.hu>
2020-10-29 01:15:20 +0100

    Install

--- install.sh
+++ install.sh
@@ -1,61 +1,77 @@
 #!/bin/bash
 
-# sudo
-if [[ $UID != 0 ]]; then
-  echo "Please run this script with sudo:"
-  echo "sudo $0 $*"
-  exit 1
-fi
+is_sudo () {
+  if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+  fi
+}
 
-# git
-git stash
-git pull
-git stash drop
+download () {
+  echo "Downloading source"
+  git stash
+  git pull
+  git stash drop
+}
 
-# mongo
-if [ ! -f /etc/apt/sources.list.d/mongodb-org-4.4.list ]; then
-  apt-get install -y gnupg wget
-  wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
-  UBUNTU_VERISON=$(cat /etc/os-release | awk -F '=' '/^VERSION_ID/{print $2}' | awk '{print $1}' | tr -d '"')
-  echo "Ubuntu $UBUNTU_VERISON"
-  case $UBUNTU_VERISON in
-    "20.04")
-      echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
-      ;;
-    "18.04")
-      echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
-      ;;
-    "16.04")
-      echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
-      ;;
-  esac
-  apt-get update
-fi
-apt-get install -y mongodb-org
-systemctl enable mongod
-systemctl start mongod
+mongo() {
+  echo "Intalling MongoDB"
+  if [ ! -f /etc/apt/sources.list.d/mongodb-org-4.4.list ]; then
+    apt-get install -y gnupg wget
+    wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
+    UBUNTU_VERISON=$(cat /etc/os-release | awk -F '=' '/^VERSION_ID/{print $2}' | awk '{print $1}' | tr -d '"')
+    echo "Ubuntu $UBUNTU_VERISON"
+    case $UBUNTU_VERISON in
+      "20.04")
+        echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+        ;;
+      "18.04")
+        echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+        ;;
+      "16.04")
+        echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+        ;;
+    esac
+    apt-get update
+  fi
+  apt-get install -y mongodb-org
+  systemctl enable mongod
+  systemctl start mongod
+}
 
-# node
-if [ ! -f nodesource_setup.sh ]; then
-  apt-get install -y curl
-  curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
-  chmod +x nodesource_setup.sh
-  ./nodesource_setup.sh
-fi
-apt-get install -y nodejs build-essential
-npm install -g npm@latest webpack@latest webpack-cli@latest
-pushd back
-npm install
-npm run build
-popd
-pushd front
-npm install
-npm run build
-popd
+node() {
+  echo "Intalling Node.js"
+  if [ ! -f nodesource_setup.sh ]; then
+    apt-get install -y curl
+    curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
+    chmod +x nodesource_setup.sh
+    ./nodesource_setup.sh
+  fi
+  apt-get install -y nodejs build-essential
+  npm install -g npm@latest webpack@latest webpack-cli@latest
+  pushd back
+  npm install
+  npm run build
+  popd
+  pushd front
+  npm install
+  npm run build
+  popd
+}
 
-# inno-comp
-systemctl stop inno-comp.service
-systemctl disable inno-comp.service
-sed -i "s/\$DIRECTORY/$(pwd | sed 's/\//\\\//g')/g" inno-comp.service
-systemctl enable $PWD/inno-comp.service
-systemctl start inno-comp.service
+application() {
+  echo "Intalling inno-comp"
+  systemctl stop inno-comp.service
+  systemctl disable inno-comp.service
+  sed -i "s/\$DIRECTORY/$(pwd | sed 's/\//\\\//g')/g" inno-comp.service
+  systemctl enable $PWD/inno-comp.service
+  systemctl start inno-comp.service
+  systemctl status inno-comp.service --no-pager -l
+}
+
+is_sudo
+download
+mongo
+node
+application
