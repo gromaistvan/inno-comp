1c5fa09edca60730a9f25fa5af9c071d0f44bdea
Groma István <Groma.IstvanDr@sdadms.hu>
2020-10-30 21:23:21 +0100

    E-mail

--- back/server.js
+++ back/server.js
@@ -3,12 +3,18 @@ const mongo = require('mongodb');
 const express = require('express');
 const bodyParser = require("body-parser");
 const multer = require('multer');
+const nodemailer = require("nodemailer");
+const { IgnorePlugin } = require('webpack');
 
 const port = process.env.PORT || 80;
 
 const database = process.env.DB || 'mongodb://localhost:27017';
 
 const app = express();
+
+app.use(bodyParser.json({ limit: '50mb', extended: true }));
+
+app.use(express.static(`${process.cwd()}/app/`));
 
 const storage = multer.diskStorage({
   destination: 'uploads/',
@@ -24,13 +30,10 @@ const storage = multer.diskStorage({
     }
   }
 })
+
 const signupUpload =
-  multer({ storage: storage, limits: { fileSize: 50*1024*1024 } })
+  multer({ storage: storage, limits: { fileSize: 50 * 1024 * 1024 } })
   .fields([{ name: 'abstract', maxCount: 1 }, { name: 'presentation', maxCount: 1 }])
-
-app.use(bodyParser.json({ limit: '50mb', extended: true }));
-
-app.use(express.static(`${process.cwd()}/app/`));
 
 app.get('/api/file', (req, res) => res.sendFile(`${path.dirname(require.main.filename)}/uploads/${req.params.file}`));
 
@@ -41,8 +44,7 @@ app.post('/api/file', (req, res) => {
       res.status(500).send(err);
     }
     else {
-      res.status(500).send(err);
-      //res.status(200).send('');
+      res.status(200).send('');
     }
   });
 });
@@ -54,17 +56,60 @@ app.get('/api/applicant', async (_req, res) => {
     const result = await client.db('inno-comp').collection('applicants').find({}).toArray();
     res.json(result);
   }
+  catch (error) {
+    console.error(error);
+  }
   finally {
     client.close();
   }
 });
+
+const transporter = process.env.GMAIL_ADDRESS && process.env.GMAIL_PASSWORD
+  ? nodemailer.createTransport(smtpTransport({
+      service: 'gmail',
+      host: 'smtp.gmail.com',
+      auth: { user: process.env.GMAIL_ADDRESS, pass: process.env.GMAIL_PASSWORD }
+    }))
+  : null;
 
 app.post('/api/applicant', async (req, res) => {
   const client = new mongo.MongoClient(database, { useUnifiedTopology: true });
   try {
     await client.connect();
     const result = await client.db('inno-comp').collection('applicants').insertOne(req.body);
+    if (transporter) {
+      await transporter.sendMail({
+        from: transporter.auth.user,
+        to: registry.body.email,
+        subject: '💡 Innovációs ösztöndíj 2020 - Regisztráció',
+        priority: 'high',
+        text: `
+Kedves ${req.body.name}!
+
+Gratulálunk, sikeresen regisztráltál az Innovációs ösztöndíj 2020 pályázatra.
+
+            _____
+           /     \
+          /       \
+         /   WWW   \
+         \    |    /
+          \   |   /
+           \  |  /
+            |===|
+            |===|
+             ###
+
+http://innovacio20.rcinet.local
+
+------------------------------
+Erre az e-mailre ne válaszolj!
+`.trim()
+      });
+    }
     res.json(result);
+  }
+  catch (error) {
+    console.error(error);
   }
   finally {
     client.close();
