1d36e32bc8128cf5a59e49e8617e40c7b1b7cc82
Groma István <Groma.IstvanDr@sdadms.hu>
2020-10-30 22:24:55 +0100

    pnpapi

--- back/webpack.config.js
+++ back/webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './server.js',
   mode: 'production',
   target: 'node',
+  plugins: [
+    new webpack.IgnorePlugin({ resourceRegExp: /pnpapi/ }),
+  ],
   output: {
     path: path.resolve(__dirname, '.'),
     filename: 'server.bundle.js'
