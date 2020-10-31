cc3acfc9debf019ed479aee92e844b5173754346
gromaistvan <wavezone@mrginfo.com>
2020-10-31 21:55:48 +0100

    Ask a question

--- resetdb.sh
+++ resetdb.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+while true; do
+  read -p "Do you wish to reset database?" yn
+  case $yn in
+    [Yy]*) break;;
+    [Nn]*) exit;;
+    *) echo "Please answer yes or no.";;
+  esac
+done
 rm -f ./back/uploads/*
 mongo inno-comp ./back/resetdb.js
