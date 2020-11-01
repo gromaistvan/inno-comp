e0eef0e5f06c6d412233c558bc939977343704c4
gromaistvan <wavezone@mrginfo.com>
2020-11-01 02:02:34 +0100

    Create database

--- back/resetdb.js
+++ back/resetdb.js
@@ -1,5 +1,21 @@
-/* eslint-disable no-undef */
-db.applicants.drop();
-db.createCollection('applicants', { autoIndexId: false });
-db.applicants.createIndex({ name: 1 }, { unique: true });
-db.applicants.createIndex({ email: 1 }, { unique: true });
+const mongodb = require('mongodb');
+
+const uri = 'mongodb://localhost:27017';
+
+const client = new mongodb.MongoClient(uri);
+
+client.connect((err) => {
+  if (err) {
+    console.error(err);
+  }
+  else {
+    const db = client.db('inno-comp');
+    const collection = db.collection('applicants');
+    collection.drop();
+    db.createCollection('applicants', { autoIndexId: false }).then(collection => {
+      collection.createIndex({ name: 1 }, { unique: true });
+      collection.createIndex({ email: 1 }, { unique: true });
+    });
+  }
+  client.close();
+});
