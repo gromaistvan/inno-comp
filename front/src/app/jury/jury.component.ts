3a7db047ed333412b96f4295b35306c8cd0d0a8e
groma.istvandr@sdadms.hu <Groma.IstvanDr@sdadms.hu>
2021-12-02 20:31:46 +0100

    Jury

--- front/src/app/jury/jury.component.ts
+++ front/src/app/jury/jury.component.ts
@@ -1,24 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Jury } from '../shared/models';
-import { UnderConstruction } from '../shared/under.construction';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-jury',
   templateUrl: './jury.component.html',
   styleUrls: ['./jury.component.css']
 })
-export class JuryComponent extends UnderConstruction {
+export class JuryComponent {
   readonly jury: Jury[] = [];
 
   constructor() {
-    super();
     this.jury.push(
     {
       name: 'Tóth Veronika', email: 'toth.ver@gmail.com', phone: '+36209391919',
       company: 'eNET',
       introduction: `A eNET Internetkutató és Tanácsadó Kft. vezető informatikai tanácsadója.`,
     });
-    return;
+    if (environment.production) return;
     this.jury.push(
     {
       name: 'Hanzlik Zsuzsa', email: 'hanzlik.zsuzsa@gmail.com', phone: '+36308608009',
