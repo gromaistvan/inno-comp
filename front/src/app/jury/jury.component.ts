3742454429e41d5aa37ff9329fd3f86d103cdcbf
groma.istvandr@sdadms.hu <Groma.IstvanDr@sdadms.hu>
2021-12-02 20:02:35 +0100

    Fix

--- front/src/app/jury/jury.component.ts
+++ front/src/app/jury/jury.component.ts
@@ -12,27 +12,24 @@ export class JuryComponent extends UnderConstruction implements OnInit {
 
   constructor() {
     super();
-    this.jury = [
-      {
-        name: 'Tóth Veronika', email: 'toth.ver@gmail.com', phone: '+36209391919',
-        company: 'eNET',
-        introduction: `A eNET Internetkutató és Tanácsadó Kft. vezető informatikai tanácsadója.`,
-      },
-      /*
-      {
-        name: 'Hanzlik Zsuzsa', email: 'hanzlik.zsuzsa@gmail.com', phone: '+36308608009',
-        company: 'Ericsson',
-        introduction: `Az Ericsson R&D-nél csoport coaching és release menedzsment területekkel foglalkozik.`
-      },
-      {
-        name: 'Király Anna', email: 'akiraly@npsh.hu', phone: '+36306570015',
-        company: 'NPSH',
-        introduction: `Novell Professzionális Szolgáltatások Magyarország Kft-nél az integrációs és fejlesztési területek vezetője.`,
-      },
-      */
-    ];
-  }
-
-  ngOnInit(): void {
+    this.jury.push(
+    {
+      name: 'Tóth Veronika', email: 'toth.ver@gmail.com', phone: '+36209391919',
+      company: 'eNET',
+      introduction: `A eNET Internetkutató és Tanácsadó Kft. vezető informatikai tanácsadója.`,
+    });
+    return;
+    this.jury.push(
+    {
+      name: 'Hanzlik Zsuzsa', email: 'hanzlik.zsuzsa@gmail.com', phone: '+36308608009',
+      company: 'Ericsson',
+      introduction: `Az Ericsson R&D-nél csoport coaching és release menedzsment területekkel foglalkozik.`
+    });
+    this.jury.push(
+    {
+      name: 'Király Anna', email: 'akiraly@npsh.hu', phone: '+36306570015',
+      company: 'NPSH',
+      introduction: `Novell Professzionális Szolgáltatások Magyarország Kft-nél az integrációs és fejlesztési területek vezetője.`,
+    });
   }
 }
