25082c9d30060e0c0569159dbc1b878a5ad1073e
groma.istvandr@sdadms.hu <Groma.IstvanDr@sdadms.hu>
2021-12-06 15:53:32 +0100

    Jury

--- front/src/app/jury/jury.component.ts
+++ front/src/app/jury/jury.component.ts
@@ -12,23 +12,35 @@ export class JuryComponent {
 
   constructor() {
     this.jury.push(
-    {
-      name: 'Tóth Veronika', email: 'toth.ver@gmail.com', phone: '+36209391919',
-      company: 'eNET',
-      introduction: `A eNET Internetkutató és Tanácsadó Kft. vezető informatikai tanácsadója.`,
-    });
-    if (environment.production) return;
+      {
+        name: 'Tóth Veronika', email: 'veronika.toth@enet.hu', phone: '+36209391919',
+        company: 'eNET',
+        introduction: 'Tóth Veronika a eNET Internetkutató és Tanácsadó Kft. vezető informatikai tanácsadója.',
+      });
     this.jury.push(
-    {
-      name: 'Hanzlik Zsuzsa', email: 'hanzlik.zsuzsa@gmail.com', phone: '+36308608009',
-      company: 'Ericsson',
-      introduction: `Az Ericsson R&D-nél csoport coaching és release menedzsment területekkel foglalkozik.`
-    });
+      {
+        name: 'Király Anna', email: 'akiraly@npsh.hu', phone: '+36306570015',
+        company: 'NPSH',
+        introduction: 'Király Anna a Novell Professzionális Szolgáltatások Magyarország Kft-nél az integrációs és fejlesztési területek vezetője.',
+      });
     this.jury.push(
-    {
-      name: 'Király Anna', email: 'akiraly@npsh.hu', phone: '+36306570015',
-      company: 'NPSH',
-      introduction: `Novell Professzionális Szolgáltatások Magyarország Kft-nél az integrációs és fejlesztési területek vezetője.`,
-    });
+      {
+        name: 'Mélykuti Balázs', email: '', phone: '+36209666569',
+        company: 'Qualysoft',
+        introduction: 'Dr. Mélykuti Balázs a Qualysoft Holding informatikai cégcsoport nemzetközi üzletfejlesztésért felelős vezetője.'
+      });
+    return;
+    this.jury.push(
+      {
+        name: 'Lovas Szabolcs', email: 'lovass.szabolcs@gmail.com', phone: '+36204644349',
+        company: '',
+        introduction: 'Lovas Szabolcs immár 7 éves szenior informatikai tanácsadói tapasztalattal rendelkezik.'
+      });
+    this.jury.push(
+      {
+        name: 'Hanzlik Zsuzsa', email: 'hanzlik.zsuzsa@gmail.com', phone: '+36308608009',
+        company: 'Ericsson',
+        introduction: 'Hanzlik Zsuzsa az Ericsson R&D-nél csoport coaching és release menedzsment területekkel foglalkozik.'
+      });
   }
 }
